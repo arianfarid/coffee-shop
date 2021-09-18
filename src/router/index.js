34f88938c737f9ede87d69fd01503946f25d7d0d
arianfarid <arian.blank.farid@gmail.com>
2021-09-17 21:28:32 -0400

    links

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/giftcards',
+    name: 'GiftCards',
+    component: () => import(/* webpackChunkName: "about" */ '../views/GiftCards.vue')
+  },
+  {
+    path: '/findstore',
+    name: 'FindStore',
+    component: () => import(/* webpackChunkName: "about" */ '../views/FindStore.vue')
   }
 ]
 
