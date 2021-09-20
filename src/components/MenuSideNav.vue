8cf7cc5d210ca773e0871bdf8206b620c7fa4b64
arianfarid <arian.blank.farid@gmail.com>
2021-09-19 21:48:38 -0400

    Side menu

---
+++ src/components/MenuSideNav.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="w-96 bg-white hidden md:flex md:flex-none pl-20 overflow-y-auto h-screen">
+    <div>
+      <div class="side-nav-item-category">
+        Drinks
+      </div>
+      <div class="side-nav-item">
+        Hot Coffees
+      </div>
+      <div class="side-nav-item">
+        Hot Teas
+      </div>
+      <div class="side-nav-item">
+        Hot Drinks
+      </div>
+      <div class="side-nav-item">
+        Frappés
+      </div>
+      <div class="side-nav-item">
+        Cold Coffees
+      </div>
+      <div class="side-nav-item">
+        Cold Teas
+      </div>
+      <div class="side-nav-item">
+        Cold Drinks
+      </div>
+      <div class="side-nav-item-category">
+        Food
+      </div>
+      <div class="side-nav-item">
+        Toasty Breakfast
+      </div>
+      <div class="side-nav-item">
+        Lunch
+      </div>
+      <div class="side-nav-item">
+        Snacks
+      </div>
+      <div class="side-nav-item">
+        Pastries
+      </div>
+      <div class="side-nav-item-category">
+        For the Home
+      </div>
+      <div class="side-nav-item">
+        Whole Bean
+      </div>
+      <div class="side-nav-item">
+        Instant
+      </div>
+      <div class="side-nav-item-category">
+        Merch
+      </div>
+      <div class="side-nav-item">
+        Tees
+      </div>
+      <div class="side-nav-item">
+        Mugs
+      </div>
+      <div class="side-nav-item">
+        Other
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.side-nav-item-category {
+  @apply pl-2 pr-2 text-lg font-semibold mt-2 mb-2;
+}
+.side-nav-item {
+  @apply pl-2 pr-2 text-base text-gray-700 mt-2 mb-2;
+}
+
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,20 +2,25 @@
   <div class="home">
     <!-- a sub menu component for featured drinks, faves, etc -->
     <sub-menu></sub-menu>
-    <!-- panel for navigating menu items -->
-    <!-- panel for drinks -->
-    Lets order coffee
+    <div class="flex">
+      <!-- panel for navigating menu items -->
+      <menu-side-nav></menu-side-nav>
+      <!-- panel for drinks -->
+      Lets order coffee
+    </div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import SubMenu from '@/components/SubMenu.vue'
+import MenuSideNav from '@/components/MenuSideNav.vue'
 
 export default {
   name: 'Home',
   components: {
+    MenuSideNav,
     SubMenu
   }
 }
+
 </script>
