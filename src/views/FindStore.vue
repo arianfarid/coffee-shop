1a8f2ae7c47ed848fd9ab90d2f39a157d1ce033c
arianfarid <arian.blank.farid@gmail.com>
2021-09-26 21:02:28 -0400

    edits

--- src/views/FindStore.vue
+++ src/views/FindStore.vue
@@ -4,18 +4,29 @@
     <sub-menu></sub-menu>
     <div class='flex'>
       <!-- panel searching stores, and listing them -->
-      <div class='grid-cols-1'>
+      <div class='hidden md:flex md:flex-none grid grid-cols-1 left-0 w-72 overflow-y-auto h-screen border-gray-200 border-r border-b rounded-br'>
+        <div class='m-4 inline'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <input type="text" name="" class="border-b border-gray-200 mr-2 transition ease-in-out hover:animate-pulse hover:border-b-2 hover:border-indigo-600 focus:border-b-2 focus:border-indigo-600 outline-none">
+          <button class="btn-pill btn-light transition ease-in-out btn-animate">Filter</button>
+        </div>
       </div>
       <!-- Map, should go on right on bigger screens, smaller height and center fill width on smaller screen -->
-      <l-map style='height:50vh'>
-        <l-geo-json :geojson='geojson' />
-        <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap" :max-zoom="10" />
-      </l-map>
+      <div class="h-36 w-full relative">
+        <div class="h-36 w-full">
+          <l-map style='height:50vh' :zoom="2" class="h-36">
+            <l-geo-json :geojson='geojson' />
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap" :max-zoom="10"></l-tile-layer>
+          </l-map>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, ref } from 'vue'
 import 'leaflet/dist/leaflet.css'
 import SubMenu from '@/components/SubMenu.vue'
 import { LMap, LGeoJson, LTileLayer } from '@vue-leaflet/vue-leaflet'
@@ -42,16 +53,20 @@ export default {
       }]
     }
     const geojsonOptions = {
-      // Options that don't rely on Leaflet methods.
+      style: {
+        color: '#34d399',
+        weight: 5,
+        opacity: 0.65
+      }
     }
     onBeforeMount(async () => {
       // HERE is where to load Leaflet components!
       const { circleMarker } = await import('leaflet/dist/leaflet-src.esm')
 
       // And now the Leaflet circleMarker function can be used by the options:
-      this.geojsonOptions.pointToLayer = (feature, latLng) =>
+      geojsonOptions.pointToLayer = (feature, latLng) =>
         circleMarker(latLng, { radius: 8 })
-      this.mapIsReady = true
+      ref.mapIsReady = true
     })
 
     return {
@@ -60,4 +75,5 @@ export default {
     }
   }
 }
+
 </script>
